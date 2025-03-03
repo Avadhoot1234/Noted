0fa59f98df60ae81e11597a8dcb15fa380e21b8c
Avadhoot1234 <avadhoot.dhumal9@gmail.com>
2025-03-03 20:09:14 +0530

    Completed with UI of login page

---
+++ client/tailwind.config.js
@@ -0,0 +1,14 @@
+
+module.exports = {
+  purge: [],
+  darkMode: true, // or 'media' or 'class'
+  theme: {
+    extend:{
+      colors:{
+        primary:"#2B85FF",
+        secondary:"#EF863E"
+      }
+    }
+  },
+  plugins: [],
+}
