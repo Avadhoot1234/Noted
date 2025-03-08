5bac624b4729f55336ee6141540a9d35c07deb91
Avadhoot1234 <avadhoot.dhumal9@gmail.com>
2025-03-08 17:28:02 +0530

    Designed the profileinfo card now working on search bar

--- client/src/components/Cards/ProfileInfo.jsx
+++ client/src/components/Cards/ProfileInfo.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
+import { getInitials } from '../../utils/helper'
 
-const ProfileInfo = () => {
+
+const ProfileInfo = ({onLogout}) => {
   return (
     <div className='flex items-center gap-3'>
-      <div className=''>AD</div>
+      <div className='w-12 h-12 
+      flex items-center justify-center 
+      rounded-full text-slate-950 font-medium
+      bg-slate-50'>{getInitials("Avadhoot Dhumal")}</div>
 
         <div>
-            <p className='text-sm font-medium'>Avadhoot</p>
-            <button className='' onClick={onLogout}>
-                Logout
+            <p className='text-sm font-medium'>Avadhoot Dhumal</p>
+            <button className='text-am text-slate-700 underline' onClick={onLogout}>
+              Logout
             </button>
         </div>
 
--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,12 +1,24 @@
 import React from 'react'
 import ProfileInfo from '../Cards/ProfileInfo'
+import {useNavigate } from 'react-router-dom';
+import Searchbar from '../Searchbar/Searchbar';
 
 const Navbar = () => {
+
+  const navigate=useNavigate();
+
+  const onLogout=()=>{
+    navigate('/login');
+  }
+
+
   return (
     <div className='bg-white flex items-center justify-between px-6 py-2 drop-shadow'>
       <h2 className='text-xl font-medium text-black py-2'>Noted</h2>
 
-      <ProfileInfo/>
+      <Searchbar/>
+
+      <ProfileInfo onLogout={onLogout}/>
 
     </div>
   )
