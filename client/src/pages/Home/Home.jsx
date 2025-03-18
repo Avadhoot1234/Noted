d496461f32686f6318fb31e456a93e0513db9dd1
Avadhoot1234 <avadhoot.dhumal9@gmail.com>
2025-03-18 13:20:08 +0530

    Working on Notecard

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import Navbar from '../../components/Navbar/Navbar'
+import NoteCard from '../../components/Cards/NoteCard'
+import {MdOutlinePushPin} from "react-icons/md";
 
 const Home = () => {
   return (
     <>
       <Navbar/>
+      <div>
+        <NoteCard title="Meeting on 4th april" 
+        date="3rd April 2024" 
+        tags="#Meeting"
+        isPinned={true}
+        onEdit={()=>{}}
+        onDelete={()=>{}}
+        onPinNote={()=>{}}
+        />
+      </div>
     </>
   )
 }
